f928662d5702de0a6ca871494fe295c42fe6db1a
Hashcode <hashcode0f@gmail.com>
2011-11-06 12:01:37 -0500

    Fix for a Phone app crash related to multiradio operation.
    Change-Id: I14f5fcc78c407ad8633c8076fb16c6e0a74f74a6

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -902,6 +902,7 @@ public class CallNotifier extends Handler
         // Release the ToneGenerator used for playing SignalInfo and CallWaiting
         if (mSignalInfoToneGenerator != null) {
             mSignalInfoToneGenerator.release();
+            mSignalInfoToneGenerator = null;
         }
 
         // Clear ringback tone player
