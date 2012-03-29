3dfe0bcf21e19df6dcc1faa83eed0c7a85088de0
Danny Baumann <dannybaumann@web.de>
2012-03-29 08:45:54 +0200

    Make PhoneToggler accept the same values as are configurable in the settings.
    It makes no sense to allow setting GSM states on a CDMA phone, and on
    GSM phones, we also want to allow setting NT_MODE_GSM_UMTS.
    
    Change-Id: Ib1fe3118020c8c02e8eb0e167aba0cb30eee6bd9

--- src/com/android/phone/PhoneToggler.java
+++ src/com/android/phone/PhoneToggler.java
@@ -45,8 +45,31 @@ public class PhoneToggler extends BroadcastReceiver  {
             	Log.d(LOG_TAG,"Got modify intent");
             }
 		if (intent.getExtras()!=null) {
-			int networkMode = intent.getExtras().getInt(NETWORK_MODE); 
-			if (networkMode == Phone.NT_MODE_GSM_ONLY || networkMode == Phone.NT_MODE_WCDMA_PREF || networkMode == Phone.NT_MODE_WCDMA_ONLY) {
+			int networkMode = intent.getExtras().getInt(NETWORK_MODE);
+            boolean networkModeOk = false;
+            int phoneType = getPhone().getPhoneType();
+
+            if (phoneType == Phone.PHONE_TYPE_GSM) {
+                if (networkMode == Phone.NT_MODE_GSM_ONLY
+                        || networkMode == Phone.NT_MODE_GSM_UMTS
+                        || networkMode == Phone.NT_MODE_WCDMA_PREF
+                        || networkMode == Phone.NT_MODE_WCDMA_ONLY) {
+                    networkModeOk = true;
+                }
+            } else if (phoneType == Phone.PHONE_TYPE_CDMA) {
+                if (networkMode == Phone.NT_MODE_CDMA
+                        || networkMode == Phone.NT_MODE_CDMA_NO_EVDO
+                        || networkMode == Phone.NT_MODE_EVDO_NO_CDMA) {
+                    networkModeOk = true;
+                }
+            }
+            if (context.getResources().getBoolean(R.bool.world_phone)) {
+                if (networkMode == Phone.NT_MODE_GLOBAL) {
+                    networkModeOk = true;
+                }
+            }
+
+            if (networkModeOk) {
                 if (DBG) {
                 	Log.d(LOG_TAG,"Will modify it to: "+networkMode);
                 }
