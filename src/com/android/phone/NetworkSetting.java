xdzou/Phone

apps/Phone: Display Short Name/MCCMNC if Long Name is empty in manual search.
The operator name for each operator obtained in the Manual Network Search
should be derived as per 3GPP spec TS 22.101 A.3(Country/PLMN indication).
While deriving the name for current operator (already registered operator), as
per the spec if NITZ name has to be displayed, then some times the long name
can be empty in MM information. Since the operator information should be derived
from a single source, MM inforamtion cannot be combined with other sources
like ME DB information for long name. In such cases, Short Name or MCCMNC string
should be displayed in the Manual Network Selection.

Change-Id: Ia9e1085ebaa0d06ae787775e3cbeffc6fcdcf76e
CRs-Fixed: 240559

src/com/android/phone/NetworkSetting.java
@@ -33,6 +33,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.telephony.CommandException;
@@ -403,7 +404,7 @@ public class NetworkSetting extends PreferenceActivity
                 // confusing mcc/mnc.
                 for (NetworkInfo ni : result) {
                     Preference carrier = new Preference(this, null);
-                    carrier.setTitle(ni.getOperatorAlphaLong());
+                    carrier.setTitle(getNetworkTitle(ni));
                     carrier.setPersistent(false);
                     mNetworkList.addPreference(carrier);
                     mNetworkMap.put(carrier, ni);
@@ -414,6 +415,25 @@ public class NetworkSetting extends PreferenceActivity
             } else {
                 displayEmptyNetworkList(true);
             }
+        }
+    }
+
+    /**
+     * Returns the title of the network obtained in the manual search.
+     *
+     * @param NetworkInfo contains the information of the network.
+     *
+     * @return Long Name if not null/empty, otherwise Short Name if not null/empty,
+     * else MCCMNC string.
+     */
+
+    private String getNetworkTitle(NetworkInfo ni) {
+        if (!TextUtils.isEmpty(ni.getOperatorAlphaLong())) {
+            return ni.getOperatorAlphaLong();
+        } else if (!TextUtils.isEmpty(ni.getOperatorAlphaShort())) {
+            return ni.getOperatorAlphaShort();
+        } else {
+            return ni.getOperatorNumeric();
         }
     }
 

CyanogenMod/android_packages_apps_Phone

Change-Id: I85f3cbd232fc4130084347aec9b4a8181b0fe896

src/com/android/phone/NetworkSetting.java
@@ -33,6 +33,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.telephony.CommandException;
@@ -408,7 +409,7 @@ public class NetworkSetting extends PreferenceActivity
                 // confusing mcc/mnc.
                 for (OperatorInfo ni : result) {
                     Preference carrier = new Preference(this, null);
-                    carrier.setTitle(ni.getOperatorAlphaLong());
+                    carrier.setTitle(getNetworkTitle(ni));
                     carrier.setPersistent(false);
                     mNetworkList.addPreference(carrier);
                     mNetworkMap.put(carrier, ni);
@@ -419,6 +420,25 @@ public class NetworkSetting extends PreferenceActivity
             } else {
                 displayEmptyNetworkList(true);
             }
+        }
+    }
+
+    /**
+     * Returns the title of the network obtained in the manual search.
+     *
+     * @param OperatorInfo contains the information of the network.
+     *
+     * @return Long Name if not null/empty, otherwise Short Name if not null/empty,
+     * else MCCMNC string.
+     */
+
+    private String getNetworkTitle(OperatorInfo ni) {
+        if (!TextUtils.isEmpty(ni.getOperatorAlphaLong())) {
+            return ni.getOperatorAlphaLong();
+        } else if (!TextUtils.isEmpty(ni.getOperatorAlphaShort())) {
+            return ni.getOperatorAlphaShort();
+        } else {
+            return ni.getOperatorNumeric();
         }
     }
 